8ea65d18b2c4a7b328f1ce6cfab98df8a1726132
SrJohn369 <srjohn963@gmail.com>
2024-06-16 22:47:48 -0300

    adicionado uma session local de iniciação do db

---
+++ app/db/database.py
@@ -0,0 +1,33 @@
+import os
+
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from databases import Database
+
+from dotenv import load_dotenv
+
+
+# carregar variáveis de ambiente
+load_dotenv()
+
+# banco de dados
+DATABASE_URL = os.getenv("DATABASE_URL")
+# --
+database = Database(DATABASE_URL)
+engine = create_engine(DATABASE_URL)
+metadata = MetaData()
+Base = declarative_base()
+
+# cria uma sessão local
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+# inicia banco
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
