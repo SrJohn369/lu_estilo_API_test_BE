bfc3bc80820dbb400745ea2be0544958843d0056
SrJohn369 <srjohn963@gmail.com>
2024-06-19 01:51:02 -0300

    revolvendo warning declarative_base()

--- app/db/database.py
+++ app/db/database.py
@@ -1,7 +1,7 @@
 import os
 
 from sqlalchemy import create_engine, MetaData
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 from databases import Database
