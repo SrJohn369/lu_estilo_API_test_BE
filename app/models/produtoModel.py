561affe7faa7b86aa9bc6aef85356005d6eb5c51
SrJohn369 <srjohn963@gmail.com>
2024-06-20 23:29:37 -0300

    criado model para Produtos

---
+++ app/models/produtoModel.py
@@ -0,0 +1,19 @@
+import uuid
+
+from sqlalchemy import Column, String, Float, Text, Integer, Date
+from app.db.database import Base
+
+
+# Cria tabela clientes
+class Produto(Base):
+    __tablename__ = "produtos"
+
+    id = Column(String(length=36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    descricao = Column(Text, nullable=False)
+    valor_de_venda = Column(Float, nullable=False)
+    cod_barra = Column(String(length=13), nullable=False)
+    secao = Column(String, nullable=False)
+    imagem = Column(String, nullable=False)
+    data_validade = Column(Date, nullable=True)
+    estoque_de_validae = Column(Integer, nullable=True)
+
