3714b10ac2ca67ee79f667459d3682b05dcbc1ff
SrJohn369 <srjohn963@gmail.com>
2024-06-21 13:53:57 -0300

    modificações, eliminando realções inexistentes

--- app/models/pedidoModel.py
+++ app/models/pedidoModel.py
@@ -11,7 +11,6 @@ class Pedido(Base):
     __tablename__ = "produtos"
 
     id = Column(String(length=36), primary_key=True, default=lambda: str(uuid.uuid4()))
-    cliente_id = Column(String, ForeignKey('clientes.id'))
     status_pedido = Column(String, nullable=False)
     data_pedido = Column(DateTime, nullable=False)
     
